ed31be0abea510334fad20b6cac7c9b94eeb3aa4
iago <iagopsilva073@gmail.com>
2026-04-30 16:12:46 -0300

    inicia o projeto

---
+++ src/test/java/com/nttdata/cliente_endereco/ClienteEnderecoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.cliente_endereco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteEnderecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
